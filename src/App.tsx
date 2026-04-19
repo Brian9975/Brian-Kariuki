aa55353c608e42c61b4047a30ea81f768ed8edff
Brian <briankariuki9975@gmail.com>
2026-04-19 17:53:44 +0300

    Added some margin to the skills text

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ const [openNav, setOpenNav] = useState(false)
   <section className="bg-slate-950 text-center items-center flex justify-center min-h-screen">
     <div className="mx-4">
       <h1 className="text-slate-100 tracking-wide  pb-5 font-extrabold text-5xl">Hello, l am <span className="text-cyan-400">Brian</span></h1>
-      <p className="text-slate-300 mx-4 pb-7 tracking-wide">A Frontend Developer building modern and scalable web applications with React, TypeScript, and Firebase.</p>
+      <p className="text-slate-300 pb-7 tracking-wide">A Frontend Developer building modern and scalable web applications with React, TypeScript, and Firebase.</p>
       <div className="tracking-wide flex justify-center gap-4">
         <a href="#projects" className="bg-cyan-400  py-2 px-4 hover:opacity-80 transition-all font-bold duration-100 rounded cursor-pointer">See My Work</a>
         <a href="#contact" className="border text-slate-300 hover:opacity-80 transition-all duration-100 cursor-pointer py-2 px-5 font-bold rounded border-slate-500">Contact Me</a>
@@ -67,7 +67,7 @@ const [openNav, setOpenNav] = useState(false)
 
   <section id="skills" className="bg-slate-950 py-30 text-center">
    <h1 className="text-2xl text-cyan-400  tracking-wide font-bold">Skills</h1>
-   <p className="text-slate-300 font-bold pt-5 pb-10">Here are technologies that l can proficiently work with</p>
+   <p className="text-slate-300 mx-2 font-bold pt-5 pb-10">Here are technologies that l can proficiently work with</p>
  <div className="mx-5 mb-30  flex flex-wrap justify-center gap-4">
     {["HTML", "CSS", "TypeScript", "React", "Firebase", "Tailwind CSS", "ShadCN UI", "Git & Github", "Vercel"].map(skill => (
       <span className="text-slate-300 bg-slate-900 border border-cyan-400 py-2 px-4 rounded-lg" key={skill}>{skill}</span>
