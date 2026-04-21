c9e5572eb04d6c0c80ccf7c55f9358a16854d4c7
Brian <briankariuki9975@gmail.com>
2026-04-21 11:06:24 +0300

    Updated the hero text

--- src/App.tsx
+++ src/App.tsx
@@ -106,7 +106,7 @@ const name = form.current!.from_name.value.trim()
   <section className="bg-slate-950 text-center items-center flex justify-center min-h-screen">
     <div className="mx-4">
       <h1 className="text-slate-100 tracking-wide  pb-5 font-extrabold text-5xl">Hello, l am <span className="text-cyan-400">Brian</span></h1>
-      <p className="text-slate-300 pb-7 tracking-wide">A Frontend Developer building modern and scalable web applications with React, TypeScript, and Firebase.</p>
+      <p className="text-slate-300 pb-7 tracking-wide">Frontend Developer specializing in React, TypeScript and Firebase web applications</p>
       <div className="tracking-wide flex justify-center gap-4">
         <a href="#projects" className="bg-cyan-400  py-2 px-4 hover:opacity-80 transition-all font-bold duration-100 rounded cursor-pointer">See My Work</a>
         <a href="#contact" className="border text-slate-300 hover:opacity-80 transition-all duration-100 cursor-pointer py-2 px-5 font-bold rounded border-slate-500">Contact Me</a>
