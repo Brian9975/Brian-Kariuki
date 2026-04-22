327ff8c5ebffc5c1220020ede5a47149f2a8b361
Brian Kariuki <briankariuki9975@gmail.com>
2026-04-22 12:08:06 +0300

    Update linkedIn url

--- src/App.tsx
+++ src/App.tsx
@@ -119,7 +119,7 @@ const name = form.current!.from_name.value.trim()
    <h1 className="text-cyan-400 text-2xl tracking-wide font-bold pt-5">About Me</h1>
    <div className="  flex justify-center items-center mb-12">
     <div className="w-180 bg-slate-950 border my-8 mx-5 px-7 py-10 tracking-wide rounded-xl border-slate-500">
-    <p className="text-slate-300 leading-relaxed text-left">l am a Frontend Web Developer based in Ongata Rongai, Kenya specializing in building business web applications using React,TypeScript,Firebase and Tailwind.
+    <p className="text-slate-300 leading-relaxed text-left">l am a Frontend Web Developer based in Nairobi, Kenya specializing in building business web applications using React,TypeScript,Firebase and Tailwind.
       l am self-taught, self-driven and l built real deployed applications that solve real business problems.l am available for freelance projects and remote opportunities world wide.
     </p>
     </div>
@@ -238,7 +238,7 @@ const name = form.current!.from_name.value.trim()
   </div>
 
    <div className="w-7">
-    <a href="" className="">
+    <a href="" className="https://www.linkedin.com/in/briankariukitech">
     <img src={LinkedIn} className="w-full" alt="LinkedIn Logo" />
     </a>
   </div>
